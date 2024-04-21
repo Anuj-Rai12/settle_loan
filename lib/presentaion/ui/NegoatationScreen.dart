1c1d583739108ff61c69cff44b219c3dfc9e20d3
Anuj-Rai12 <anujrai04082002.0@gmail.com>
2024-04-21 16:19:13 +0530

    Added the Negation status:Successfully

--- lib/presentaion/ui/NegoatationScreen.dart
+++ lib/presentaion/ui/NegoatationScreen.dart
@@ -29,122 +29,129 @@ class _NegotiationScreenState extends State<NegotiationScreen> {
                   fontWeight: FontWeight.bold,
                   fontFamily: publicSansBold)),
           centerTitle: true),
-      body: ListView.builder(
-          shrinkWrap: true,
-          physics: const ClampingScrollPhysics(),
-          itemCount: NegotiationScreenModel.ls.length,
-          itemBuilder: (context, position) {
-            var data = NegotiationScreenModel.ls[position];
-            return Container(
-              decoration: const BoxDecoration(
-                color: Color(editTextBg),
-                borderRadius: BorderRadius.all(Radius.circular(10))
-              ),
-              margin: const EdgeInsets.only(left: 16,right: 16,top: 12),
-              child: InkWell(
-                  onTap: () {},
-                  child: Container(
-                    margin: const EdgeInsets.only(
-                        left: 16, right: 16, top: 16, bottom: 16),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          width: 56,
-                          height: 56,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            // This makes the corners circular
-                            child: Image.asset(
-                              data.src,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 16, right: 16),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(data.bankName,
-                                    style: const TextStyle(
-                                        fontFamily: publicSansBold,
-                                        color: Color(textColor),
-                                        fontSize: 18)),
-                                Container(
-                                  margin: const EdgeInsets.only(top: 8),
-                                  child: const Text(
-                                      "Possibility of Settlement Read.",
-                                      maxLines: 3,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                          fontFamily: publicSansReg,
-                                          color: Color(textColor),
-                                          fontSize: 16)),
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.only(top: 8),
-                                  child: Text("$Rupess_Symbol ${data.possibilty}",
-                                      maxLines: 3,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: const TextStyle(
-                                          fontFamily: publicSansReg,
-                                          color: Color(editTextColor),
-                                          fontSize: 14)),
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.only(top: 8),
-                                  child: const Text(
-                                      "Status of amt. as of agreement",
-                                      maxLines: 3,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                          fontFamily: publicSansReg,
-                                          color: Color(textColor),
-                                          fontSize: 16)),
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.only(top: 8),
-                                  child: Text(
-                                      "$Rupess_Symbol ${data.statusAggrement}",
-                                      maxLines: 3,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: const TextStyle(
-                                          fontFamily: publicSansReg,
-                                          color: Color(editTextColor),
-                                          fontSize: 14)),
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.only(top: 8),
-                                  child: const Text(
-                                      "Current amount offered (including  o/s)",
-                                      maxLines: 3,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                          fontFamily: publicSansReg,
-                                          color: Color(textColor),
-                                          fontSize: 16)),
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.only(top: 8),
-                                  child: Text("$Rupess_Symbol ${data.current}",
-                                      maxLines: 3,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: const TextStyle(
-                                          fontFamily: publicSansReg,
-                                          color: Color(editTextColor),
-                                          fontSize: 14)),
-                                ),
-                              ],
-                            ),
-                          ),
-                        )
-                      ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.builder(
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                itemCount: NegotiationScreenModel.ls.length,
+                itemBuilder: (context, position) {
+                  var data = NegotiationScreenModel.ls[position];
+                  return Container(
+                    decoration: const BoxDecoration(
+                        color: Color(editTextBg),
+                        borderRadius: BorderRadius.all(Radius.circular(10))
                     ),
-                  )),
-            );
-          }),
+                    margin: const EdgeInsets.only(left: 16,right: 16,top: 12),
+                    child: InkWell(
+                        onTap: () {},
+                        child: Container(
+                          margin: const EdgeInsets.only(
+                              left: 16, right: 16, top: 16, bottom: 16),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                width: 56,
+                                height: 56,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  // This makes the corners circular
+                                  child: Image.asset(
+                                    data.src,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 16, right: 16),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(data.bankName,
+                                          style: const TextStyle(
+                                              fontFamily: publicSansBold,
+                                              color: Color(textColor),
+                                              fontSize: 18)),
+                                      Container(
+                                        margin: const EdgeInsets.only(top: 8),
+                                        child: const Text(
+                                            "Possibility of Settlement Read.",
+                                            maxLines: 3,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: TextStyle(
+                                                fontFamily: publicSansReg,
+                                                color: Color(textColor),
+                                                fontSize: 16)),
+                                      ),
+                                      Container(
+                                        margin: const EdgeInsets.only(top: 8),
+                                        child: Text("$Rupess_Symbol ${data.possibilty}",
+                                            maxLines: 3,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: const TextStyle(
+                                                fontFamily: publicSansReg,
+                                                color: Color(editTextColor),
+                                                fontSize: 14)),
+                                      ),
+                                      Container(
+                                        margin: const EdgeInsets.only(top: 8),
+                                        child: const Text(
+                                            "Status of amt. as of agreement",
+                                            maxLines: 3,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: TextStyle(
+                                                fontFamily: publicSansReg,
+                                                color: Color(textColor),
+                                                fontSize: 16)),
+                                      ),
+                                      Container(
+                                        margin: const EdgeInsets.only(top: 8),
+                                        child: Text(
+                                            "$Rupess_Symbol ${data.statusAggrement}",
+                                            maxLines: 3,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: const TextStyle(
+                                                fontFamily: publicSansReg,
+                                                color: Color(editTextColor),
+                                                fontSize: 14)),
+                                      ),
+                                      Container(
+                                        margin: const EdgeInsets.only(top: 8),
+                                        child: const Text(
+                                            "Current amount offered (including  o/s)",
+                                            maxLines: 3,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: TextStyle(
+                                                fontFamily: publicSansReg,
+                                                color: Color(textColor),
+                                                fontSize: 16)),
+                                      ),
+                                      Container(
+                                        margin: const EdgeInsets.only(top: 8),
+                                        child: Text("$Rupess_Symbol ${data.current}",
+                                            maxLines: 3,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: const TextStyle(
+                                                fontFamily: publicSansReg,
+                                                color: Color(editTextColor),
+                                                fontSize: 14)),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        )),
+                  );
+                }),
+            const SizedBox(height: 60)
+          ],
+        ),
+      ),
     ));
   }
 }
