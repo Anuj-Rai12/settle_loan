c97bf3912493bf6beaa1d07b48c05e145fccee9b
Anuj-Rai12 <anujrai04082002.0@gmail.com>
2024-04-27 01:40:51 +0530

    Updated the AnyTimeLawyer issue status:Successfully

--- lib/presentaion/ui/AnyTimeLawyer.dart
+++ lib/presentaion/ui/AnyTimeLawyer.dart
@@ -6,6 +6,7 @@ import 'package:loansettle/utils/BlocEvent.dart';
 import 'package:loansettle/utils/NewTbUrl.dart';
 import 'package:loansettle/values/res/Resources.dart';
 import 'package:sealed_flutter_bloc/sealed_flutter_bloc.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../../utils/ApiWrapperResponse.dart';
 import '../../utils/FilesUtils.dart';
 import '../../utils/SealedState.dart';
@@ -29,7 +30,6 @@ class _AnyTimeLawyerState extends State<AnyTimeLawyer> {
     super.initState();
     _viewModel = BlocProvider.of<BenefitOfPermiumViewModel>(context);
     _viewModel?.add(DataRequested(data: null));
-
   }
 
   @override
@@ -37,20 +37,19 @@ class _AnyTimeLawyerState extends State<AnyTimeLawyer> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SealedBlocBuilder4<BenefitOfPermiumViewModel, SealedState, Inital,
-          Loading, Success, Error>(
+              Loading, Success, Error>(
           builder: (context, state) => state((initial) {
-            return loading();
-          }, (load) {
-            return loading();
-          }, (success) {
-            return sucessBody(success.data as List<BenfitPermiumInstance>);
-          }, (e) {
-            return error(
-                isValidString(e.error) ? e.e.toString() : e.error!);
-          })),
+                return loading();
+              }, (load) {
+                return loading();
+              }, (success) {
+                return sucessBody(success.data as List<BenfitPermiumInstance>);
+              }, (e) {
+                return error(
+                    isValidString(e.error) ? e.e.toString() : e.error!);
+              })),
     );
   }
-
 
   Widget loading() {
     return const Center(child: CircularProgressIndicator());
@@ -70,55 +69,95 @@ class _AnyTimeLawyerState extends State<AnyTimeLawyer> {
     );
   }
 
-  Widget sucessBody(List<BenfitPermiumInstance> data){
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.asset(
-                anytimeLawyerIMG,
-                width: MediaQuery.of(context).size.width,
-                height: 320,
-                fit: BoxFit.fitWidth,
-              ),
-              Container(
-                padding: const EdgeInsets.only(
-                    left: 16, right: 16, bottom: 12),
-                child: const Text("Anytime Lawyer",
-                    style: TextStyle(
-                      color: Color(textColor),
-                      fontFamily: publicSansBold,
-                      fontSize: 28,
-                    )),
-              ),
-              Container(
-                padding:
-                const EdgeInsets.only(left: 16, right: 16, bottom: 16),
-                child: const Text(
-                    "Get access to legal assistance for you and your family. Our team of lawyers can help with a wide  range of issues.",
-                    style: TextStyle(
-                      color: Color(editTextColor),
-                      fontFamily: publicSansReg,
-                      fontSize: 14,
-                    )),
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 16, right: 16, top: 16),
-                child: const Text("What's Included",
-                    style: TextStyle(
-                      color: Color(textColor),
-                      fontFamily: publicSansBold,
-                      fontSize: 18,
-                    )),
-              ),
-              anyTimeAdaptor(data, context)
-            ],
+  Widget sucessBody(List<BenfitPermiumInstance> data) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          expandedHeight: 320.0,
+          floating: false,
+          pinned: true,
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.white,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Image.asset(
+              anytimeLawyerIMG,
+              width: MediaQuery.of(context).size.width,
+              height: 320,
+              fit: BoxFit.fitWidth,
+            ),
           ),
-          Container(
-            margin: const EdgeInsets.only(
-                left: 16, right: 16, top: 12, bottom: 12),
+        ),
+        SliverToBoxAdapter(
+          child: Container(
+            padding: const EdgeInsets.only(left: 16, right: 16, bottom: 12,top: 12),
+            child: const Text("Anytime Lawyer",
+                style: TextStyle(
+                  color: Color(textColor),
+                  fontFamily: publicSansBold,
+                  fontSize: 28,
+                )),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Container(
+            padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+            child: const Text(
+                "Get access to legal assistance for you and your family. Our team of lawyers can help with a wide  range of issues.",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: publicSansReg,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold)),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Container(
+            padding: const EdgeInsets.only(left: 16, right: 16, top: 16),
+            child: const Text("What's Included",
+                style: TextStyle(
+                  color: Color(textColor),
+                  fontFamily: publicSansBold,
+                  fontSize: 19,
+                )),
+          ),
+        ),
+        anyTimeAdaptor(data, context),
+        SliverToBoxAdapter(
+          child: Container(
+            margin:
+            const EdgeInsets.only(left: 16, right: 16, top: 12, bottom: 12),
+            child: TextButton(
+                onPressed: ()  async {
+                  String email = Uri.encodeComponent('finane@settleloan.in');
+                  String subject = Uri.encodeComponent('ATL subscription');
+                  Uri uri = Uri.parse('mailto:$email?subject=$subject');
+
+                  if (await canLaunch(uri.toString())) {
+                    await launch(uri.toString());
+                  } else {
+                    print('Could not launch $uri');
+                  }
+                },
+                style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)),
+                    minimumSize: const Size(double.infinity, 55),
+                    backgroundColor: const Color(buttonColor)),
+                child: const Text(
+                  "Subscription Plan",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: publicSansBold,
+                      color: Colors.white),
+                  textAlign: TextAlign.center,
+                )),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Container(
+            margin:
+                const EdgeInsets.only(left: 16, right: 16, top: 12, bottom: 12),
             child: TextButton(
                 onPressed: () {
                   debugLogs("text button clicked");
@@ -130,16 +169,19 @@ class _AnyTimeLawyerState extends State<AnyTimeLawyer> {
                     minimumSize: const Size(double.infinity, 55),
                     backgroundColor: const Color(buttonColor)),
                 child: const Text(
-                  "Try Now!!",
+                  "Explore",
                   style: TextStyle(
                       fontSize: 16,
                       fontFamily: publicSansBold,
                       color: Colors.white),
                   textAlign: TextAlign.center,
                 )),
-          )
-        ],
-      ),
+          ),
+        ),
+        const SliverToBoxAdapter(
+          child: SizedBox(height: 40),
+        )
+      ],
     );
   }
 }
--- lib/presentaion/ui/adaptor/AnyTimeAdaptor.dart
+++ lib/presentaion/ui/adaptor/AnyTimeAdaptor.dart
@@ -4,44 +4,47 @@ import '../../../values/color/Colors.dart';
 import '../../../values/fonts/Fonts.dart';
 
 Widget anyTimeAdaptor(List<BenfitPermiumInstance> arr, BuildContext context) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      ListView.builder(
-          shrinkWrap: true,
-          physics: const ClampingScrollPhysics(),
-          itemCount: arr.length,
-          itemBuilder: (context, position) {
-            var data = arr[position];
-            return Container(
-              padding: const EdgeInsets.only(
-                  left: 16, right: 16, bottom: 16),
-              child: Wrap(
-                crossAxisAlignment: WrapCrossAlignment.start,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(data.title??"",
-                          style: const TextStyle(
-                              fontFamily: publicSansBold,
-                              color: Color(textColor),
-                              fontSize: 16)),
-
-                      Container(
-                        margin: const EdgeInsets.only(top: 5),
-                        child: Text(data.description??"",
+  return SliverToBoxAdapter(
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ListView.builder(
+            shrinkWrap: true,
+            physics: const ClampingScrollPhysics(),
+            itemCount: arr.length,
+            itemBuilder: (context, position) {
+              var data = arr[position];
+              return Container(
+                padding: const EdgeInsets.only(
+                    left: 16, right: 16, bottom: 16),
+                child: Wrap(
+                  crossAxisAlignment: WrapCrossAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(data.title??"",
                             style: const TextStyle(
-                                fontFamily: publicSansReg,
-                                color: Color(editTextColor),
-                                fontSize: 14)),
-                      )
-                    ],
-                  )
-                ]
-              ),
-            );
-          }),
-    ],
+                                fontFamily: publicSansBold,
+                                color: Color(textColor),
+                                fontSize: 16)),
+
+                        Container(
+                          margin: const EdgeInsets.only(top: 5),
+                          child: Text(data.description??"",
+                              style: const TextStyle(
+                                  fontFamily: publicSansReg,
+                                  color: Color(editTextColor),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14)),
+                        )
+                      ],
+                    )
+                  ]
+                ),
+              );
+            }),
+      ],
+    ),
   );
 }
