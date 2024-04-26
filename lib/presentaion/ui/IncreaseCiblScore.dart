434fe3c5945cabb85519d3fa995d13d0be582203
Anuj-Rai12 <anujrai04082002.0@gmail.com>
2024-04-26 23:40:06 +0530

    Change the IncreaseCiblScore status:Successfully

--- lib/presentaion/ui/IncreaseCiblScore.dart
+++ lib/presentaion/ui/IncreaseCiblScore.dart
@@ -33,144 +33,131 @@ class _IncreaseCIBLScoreState extends State<IncreaseCIBLScore> {
       body: Column(
         children: [
           Container(
-            padding:
-                const EdgeInsets.only(left: 8, right: 16, bottom: 12, top: 12),
-            child: Container(
-              margin:
-                  const EdgeInsets.only(left: 16, right: 16, bottom: 0, top: 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("CiBl Score",
-                      style: TextStyle(
-                          color: Color(textColor),
-                          fontFamily: publicSansReg,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold)),
-                  Container(
-                    margin: const EdgeInsets.only(top: 8),
-                    padding: const EdgeInsets.only(
-                        top: 6, bottom: 6, left: 16, right: 16),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: const Color(editTextBg)),
-                    width: MediaQuery.of(context).size.width,
-                    child: TextFormField(
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                      autofocus: true,
-                      style: const TextStyle(
-                          color: Color(editTextColor),
-                          fontFamily: publicSansReg,
-                          fontSize: 16),
-                      decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Enter Your CiBle Score",
-                          hintStyle: TextStyle(
-                              color: Color(editTextColor),
-                              fontFamily: publicSansReg,
-                              fontSize: 16)),
-                      onChanged: (value) {
-                        // get the value
-                      },
-                    ),
-                  )
-                ],
-              ),
+            margin: const EdgeInsets.only(left: 16, right: 16, top: 35, bottom: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("CiBl Score",
+                    style: TextStyle(
+                        color: Color(textColor),
+                        fontFamily: publicSansReg,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold)),
+                Container(
+                  margin: const EdgeInsets.only(top: 16),
+                  padding: const EdgeInsets.only(left: 16, right: 16),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(
+                          color: const Color(borderTint), width: 1)),
+                  width: MediaQuery.of(context).size.width,
+                  height: 56,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    autofocus: true,
+                    style: const TextStyle(
+                        color: Colors.black,
+                        fontFamily: publicSansReg,
+                        fontSize: 16),
+                    decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Enter Your CiBl Score",
+                        hintStyle: TextStyle(
+                            color: Color(borderTint),
+                            fontFamily: publicSansReg,
+                            fontSize: 16)),
+                    onChanged: (value) {
+                      // get the value
+                     // _email = value;
+                    },
+                  ),
+                )
+              ],
             ),
           ),
           Container(
-            padding:
-                const EdgeInsets.only(left: 8, right: 16, bottom: 12, top: 12),
-            child: Container(
-              margin:
-                  const EdgeInsets.only(left: 16, right: 16, bottom: 0, top: 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("Yours Target Score",
-                      style: TextStyle(
-                          color: Color(textColor),
-                          fontFamily: publicSansReg,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold)),
-                  Container(
-                    margin: const EdgeInsets.only(top: 8),
-                    padding: const EdgeInsets.only(
-                        top: 6, bottom: 6, left: 16, right: 16),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: const Color(editTextBg)),
-                    width: MediaQuery.of(context).size.width,
-                    child: TextFormField(
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                      autofocus: true,
-                      style: const TextStyle(
-                          color: Color(editTextColor),
-                          fontFamily: publicSansReg,
-                          fontSize: 16),
-                      decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Enter Your Target Score",
-                          hintStyle: TextStyle(
-                              color: Color(editTextColor),
-                              fontFamily: publicSansReg,
-                              fontSize: 16)),
-                      onChanged: (value) {
-                        // get the value
-                      },
-                    ),
-                  )
-                ],
-              ),
+            margin: const EdgeInsets.only(left: 16, right: 16, top: 12, bottom: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Yours Target Score",
+                    style: TextStyle(
+                        color: Color(textColor),
+                        fontFamily: publicSansReg,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold)),
+                Container(
+                  margin: const EdgeInsets.only(top: 16),
+                  padding: const EdgeInsets.only(left: 16, right: 16),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(
+                          color: const Color(borderTint), width: 1)),
+                  width: MediaQuery.of(context).size.width,
+                  height: 56,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    autofocus: true,
+                    style: const TextStyle(
+                        color: Colors.black,
+                        fontFamily: publicSansReg,
+                        fontSize: 16),
+                    decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Enter Your Target Score",
+                        hintStyle: TextStyle(
+                            color: Color(borderTint),
+                            fontFamily: publicSansReg,
+                            fontSize: 16)),
+                    onChanged: (value) {
+                      // get the value
+                    },
+                  ),
+                )
+              ],
             ),
           ),
           Container(
-            padding:
-                const EdgeInsets.only(left: 8, right: 16, bottom: 12, top: 12),
-            child: Container(
-              margin:
-                  const EdgeInsets.only(left: 16, right: 16, bottom: 0, top: 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("Timeframe (Months)",
-                      style: TextStyle(
-                          color: Color(textColor),
-                          fontFamily: publicSansReg,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold)),
-                  Container(
-                    margin: const EdgeInsets.only(top: 8),
-                    padding: const EdgeInsets.only(
-                        top: 6, bottom: 6, left: 16, right: 16),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: const Color(editTextBg)),
-                    width: MediaQuery.of(context).size.width,
-                    child: TextFormField(
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                      autofocus: true,
-                      style: const TextStyle(
-                          color: Color(editTextColor),
-                          fontFamily: publicSansReg,
-                          fontSize: 16),
-                      decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Add your Timeframe (Month)",
-                          hintStyle: TextStyle(
-                              color: Color(editTextColor),
-                              fontFamily: publicSansReg,
-                              fontSize: 16)),
-                      onChanged: (value) {
-                        // get the value
-                      },
-                    ),
-                  )
-                ],
-              ),
+            margin: const EdgeInsets.only(left: 16, right: 16, top: 12, bottom: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Timeframe (Months)",
+                    style: TextStyle(
+                        color: Color(textColor),
+                        fontFamily: publicSansReg,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold)),
+                Container(
+                  margin: const EdgeInsets.only(top: 16),
+                  padding: const EdgeInsets.only(left: 16, right: 16),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(
+                          color: const Color(borderTint), width: 1)),
+                  width: MediaQuery.of(context).size.width,
+                  height: 56,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    autofocus: true,
+                    style: const TextStyle(
+                        color: Colors.black,
+                        fontFamily: publicSansReg,
+                        fontSize: 16),
+                    decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Add your Timeframe (Month)",
+                        hintStyle: TextStyle(
+                            color: Color(borderTint),
+                            fontFamily: publicSansReg,
+                            fontSize: 16)),
+                    onChanged: (value) {
+                      // get the value
+                    },
+                  ),
+                )
+              ],
             ),
           ),
           Container(
